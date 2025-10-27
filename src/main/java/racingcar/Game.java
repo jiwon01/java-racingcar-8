7954c0fad3aa96b9dd9f9b60f798b9374180ab14
jiwonk <jiwonk12@icloud.com>
2025-10-27 16:18:18 +0900

    feat: 자동차 목록 입력 및 저장

--- src/main/java/racingcar/Game.java
+++ src/main/java/racingcar/Game.java
@@ -1,7 +1,11 @@
 package racingcar;
 
 public class Game {
-    public static void Game() {
+    public void StartGame() {
+        // 자동차 이름 목록 입력
+        String userInput = camp.nextstep.edu.missionutils.Console.readLine();
+        String[] carNames = userInput.split(",");
 
+        // 유효성 검증
     }
 }
